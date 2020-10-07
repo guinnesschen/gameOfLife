30ebb820d68e2516e67c07374cda581154b12584
guinnesschen <guinness.chen@gmail.com>
2020-10-07 13:02:16 +0800

    add build

---
+++ build/precache-manifest.0624a57e10fb3ea6230352920ba3fc41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca9b83a07ac94f1bf5f94fd573539cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209317efd8af5333ec4a",
+    "url": "/static/css/main.8c5b4b6d.chunk.css"
+  },
+  {
+    "revision": "fb7b2bf9766e1741d988",
+    "url": "/static/js/2.c033792c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c033792c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209317efd8af5333ec4a",
+    "url": "/static/js/main.20f296b0.chunk.js"
+  },
+  {
+    "revision": "a98384c36e9903635e0d",
+    "url": "/static/js/runtime-main.4357a549.js"
+  }
+]);
