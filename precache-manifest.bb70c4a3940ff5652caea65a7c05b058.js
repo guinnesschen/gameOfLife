a0be0b79aba65df8e345f8dfcd2ed8b04af56dae
guinnesschen <guinness.chen@gmail.com>
2020-10-07 13:16:15 +0800

    Updates

---
+++ precache-manifest.bb70c4a3940ff5652caea65a7c05b058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b1875c2299cd715f6a867d149f7d38",
+    "url": "/gameOfLife/index.html"
+  },
+  {
+    "revision": "209317efd8af5333ec4a",
+    "url": "/gameOfLife/static/css/main.8c5b4b6d.chunk.css"
+  },
+  {
+    "revision": "2a1bd9ce26228985761f",
+    "url": "/gameOfLife/static/js/2.e92c268d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gameOfLife/static/js/2.e92c268d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209317efd8af5333ec4a",
+    "url": "/gameOfLife/static/js/main.20f296b0.chunk.js"
+  },
+  {
+    "revision": "68f5f6ac7da99367952e",
+    "url": "/gameOfLife/static/js/runtime-main.edfc6f60.js"
+  }
+]);
